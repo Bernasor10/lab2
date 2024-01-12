44d54b740b794dbd81698cd5235f5562ffa68e41
Ronaldo Bernasor II <rsbernasor2@student.apc.edu.ph>
2024-01-13 03:10:53 +0800

    Made a better scrolling experience by making it smoother.

--- week3/script.js
+++ week3/script.js
@@ -2,27 +2,28 @@ document.addEventListener('DOMContentLoaded', function() {
     var menuToggle = document.querySelector('.menu-toggle');
     var sidebar = document.getElementById("mySidebar");
     var body = document.body; // Reference to the body element
+    var logo = document.getElementById('logo'); // Reference to the logo element
 
-// Function to open the sidebar
-function openSidebar() {
-    sidebar.style.width = "250px";
-    body.classList.add('body-no-scroll'); // Disable scrolling
-    menuToggle.classList.add('active');
-    document.addEventListener('touchmove', preventDefault, { passive: false });
-}
+    // Function to open the sidebar
+    function openSidebar() {
+        sidebar.style.width = "250px";
+        body.classList.add('body-no-scroll'); // Disable scrolling
+        menuToggle.classList.add('active');
+        document.addEventListener('touchmove', preventDefault, { passive: false });
+    }
 
-// Function to close the sidebar
-function closeSidebar() {
-    sidebar.style.width = "0";
-    body.classList.remove('body-no-scroll'); // Enable scrolling
-    menuToggle.classList.remove('active');
-    document.removeEventListener('touchmove', preventDefault, { passive: false });
-}
+    // Function to close the sidebar
+    function closeSidebar() {
+        sidebar.style.width = "0";
+        body.classList.remove('body-no-scroll'); // Enable scrolling
+        menuToggle.classList.remove('active');
+        document.removeEventListener('touchmove', preventDefault, { passive: false });
+    }
 
-// Prevent default function
-function preventDefault(e) {
-    e.preventDefault();
-}
+    // Prevent default function
+    function preventDefault(e) {
+        e.preventDefault();
+    }
 
     // Toggle the sidebar on click
     menuToggle.addEventListener('click', function() {
@@ -47,4 +48,30 @@ function preventDefault(e) {
             closeSidebar();
         }
     });
+
+    // Refresh the page and go to the home section when the logo is clicked
+    logo.addEventListener('click', function(event) {
+        event.preventDefault();
+        localStorage.setItem('navigateToHome', 'true');
+        location.reload(); // refresh the page
+    });
+
+    if (localStorage.getItem('navigateToHome') === 'true') {
+        location.href = '#home'; // go to the home section
+        localStorage.removeItem('navigateToHome'); // clear the flag
+    }
+
+    $('a[href^="#"]').on('click', function(e) {
+        e.preventDefault();
+    
+        var target = this.hash;
+        var $target = $(target);
+    
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 1000, 'swing', function() {
+            window.location.hash = target;
+        });
+    });
+    
 });
