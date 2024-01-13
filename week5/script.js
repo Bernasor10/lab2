355f8ac9d194b832cef7c04d2fed27212c54fe5d
Ronaldo Bernasor II <rsbernasor2@student.apc.edu.ph>
2024-01-14 01:57:19 +0800

    Part 1: Lessons 1-8: Introduction, Variables, Let, Const & Data Types, Booleans, Operators, Assignment, Arithmetic & Math, Strings, Dates, and Events, Precedence & Conditionals.

---
+++ week5/script.js
@@ -0,0 +1,85 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var menuToggle = document.querySelector('.menu-toggle');
+    var sidebar = document.getElementById("mySidebar");
+    var body = document.body; // Reference to the body element
+    var logo = document.getElementById('logo'); // Reference to the logo element
+
+    // Function to open the sidebar
+    function openSidebar() {
+        sidebar.style.width = "250px";
+        body.classList.add('body-no-scroll'); // Disable scrolling
+        menuToggle.classList.add('active');
+        document.addEventListener('touchmove', preventDefault, { passive: false });
+    }
+
+    // Function to close the sidebar
+    function closeSidebar() {
+        sidebar.style.width = "0";
+        body.classList.remove('body-no-scroll'); // Enable scrolling
+        menuToggle.classList.remove('active');
+        document.removeEventListener('touchmove', preventDefault, { passive: false });
+    }
+
+    // Prevent default function
+    function preventDefault(e) {
+        e.preventDefault();
+    }
+
+    // Toggle the sidebar on click
+    menuToggle.addEventListener('click', function() {
+        var isOpen = sidebar.style.width === "250px";
+        if (isOpen) {
+            closeSidebar();
+        } else {
+            openSidebar();
+        }
+    });
+
+    // Close the sidebar by clicking the close button
+    var closeButton = document.querySelector('.closebtn');
+    closeButton.addEventListener('click', closeSidebar);
+
+    // Close the sidebar by clicking outside of it
+    document.addEventListener('click', function(event) {
+        var isClickInsideSidebar = sidebar.contains(event.target);
+        var isClickInsideMenuToggle = menuToggle.contains(event.target);
+
+        if (!isClickInsideSidebar && !isClickInsideMenuToggle && sidebar.style.width === '250px') {
+            closeSidebar();
+        }
+    });
+
+    $('a[href^="#"]').on('click', function(e) {
+        e.preventDefault();
+    
+        var target = this.hash;
+        var $target = $(target);
+    
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 1000, 'swing', function() {
+            window.location.hash = target;
+        });
+    });
+
+var timeElement = document.getElementById('time');
+
+// Function to get user's local time, date, and timezone
+function displayLocalTimeAndTimezone() {
+    var date = new Date();
+    var localTime = date.toLocaleTimeString();
+    var localDate = date.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
+
+    // Get timezone
+    var timeZone = /\((.*)\)/.exec(date.toString())[1];
+
+    timeElement.textContent = 'Local date: ' + localDate + ', Local time: ' + localTime + ', Timezone: ' + timeZone;
+}
+
+displayLocalTimeAndTimezone();
+
+// Update the time every second
+setInterval(displayLocalTimeAndTimezone, 1000);
+
+
+});
